f45cc53b58ce9c55ab8cd2541fb5fe00976fe2f7
Andy Davies <1andydavies1@gmail.com>
2016-05-12 17:21:33 +0100

    Prevent API call when search area too large, and alert user

--- js/map.js
+++ js/map.js
@@ -10,6 +10,12 @@ var initOptions = {
 // zoom level once a place is searched for (street level)
 var streetZoom = 15
 
+// minimum (widest) zoom for stops API to be called (prevents too wide an area returning too much data)
+var minimum_zoom = 11;
+
+// alert to user when zoom too wide
+var alertMessage = 'Zoom is too wide, zoom in to see stop data';
+
 // color of icons
 var redIcon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
 var blueIcon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
@@ -39,14 +45,8 @@ searchBox.addListener('places_changed', function() {
 	var places = searchBox.getPlaces();
 
 	// select places[0] TODO: why? will this be correct every time?
-	var place = places[0];
-
-	// TESTS
-	//console.log(places);
-	//console.log(place.geometry.viewport);
-	//console.log("NEWTEST: " + place.geometry.location);	
-
 	// TODO: need error checking here. Maybe test if ...geometry.location exists
+	var place = places[0];		
 
 	// set viewport on selected place and zoom in
 	map.setCenter(place.geometry.location);
@@ -56,17 +56,27 @@ searchBox.addListener('places_changed', function() {
 	var searchMarker = addMarker(place.geometry.location, redIcon);	
 })
 
-
-
-// when bounds change (when user scrolls map OR when changed via search as above)
+// when bounds change (when user scrolls map OR when changed via search as above)...
 map.addListener('bounds_changed', function() {
 	
-	// query API
-	getStopsData()
+	// ...query API, but not if map zoomed out too much - to prevent too many data	
+	if (map.zoom <= minimum_zoom) {
 
-	// TEST
-	//console.log(map.getBounds().getSouthWest().lat());
+		// alert user
+		document.getElementById('alert').innerHTML = alertMessage;
+
+	}
+	else {
+
+		// clear any previous alert
+		document.getElementById('alert').innerHTML = '';
+
+		// call police.uk API
+		getStopsData();
+	}	
 })
+
+/*-------------FUNCTIONS------------------------------------------------*/
 
 function getStopsData() {
 	// clear previous stop markers to stop overloading
@@ -75,9 +85,7 @@ function getStopsData() {
     	stopMarkers[i].setMap(null);
     } 
     // clear array of markers altogether
-	stopMarkers = [];
-
-	
+	stopMarkers = [];	
 
 	// get current bounds from getBounds object
 	var current_bounds = map.getBounds();
@@ -85,11 +93,6 @@ function getStopsData() {
 	var min_lng = current_bounds.getSouthWest().lng();
 	var max_lat = current_bounds.getNorthEast().lat();
 	var max_lng = current_bounds.getNorthEast().lng();
-
-	// TEST
-	//console.log(current_bounds);
-	console.log(min_lat, min_lng, max_lat, max_lng);
-	//console.log(place.geometry.viewport);
 
 	/* TEST: add markers at boundary points
 	addMarker(current_bounds.getSouthWest(), blueIcon);
@@ -102,130 +105,24 @@ function getStopsData() {
 	var poly_4 = max_lat.toString() + ',' + min_lng.toString();
 	var query = 'https://data.police.uk/api/stops-street?poly=' + poly_1 + poly_2 + poly_3 + poly_4;
 
-	// test query string
-	console.log(query);
-
 	// query API. Creates JSON object of 'stops'
 	var request = new XMLHttpRequest();
 	request.open("GET", query); // async by default
 	request.addEventListener("load", function() {
-		// once loaded, do this..
-
+		
+		// once loaded, parse the json string
 		stops = JSON.parse(request.responseText);
 
-		// test
-		//console.log(stops);
-
+		// add stops markers to map
 		addStopsMarkers(stops);
 	});
-	request.send(null);
-
-	
+	// http://stackoverflow.com/questions/15123839/why-do-we-pass-null-to-xmlhttprequest-send
+	request.send(null);	
 }
 
-// addSearchListener(searchBox);
-
-
-
-
-
-
-/*function addSearchListener(searchBox) {
-	// listen for user selecting place
-	searchBox.addListener('places_changed', function() {
-		// getPlaces() return an ARRAY of possible matches
-		// so "li" returns array of possibles
-		var places = searchBox.getPlaces();
-		// just select 1st place in array
-		var place = places[0];
-
-		// create new marker
-		var newMarkOptions = {
-			map : map,
-			title : place.formatted_address,
-			position : place.geometry.location
-		};
-		addMarker(newMarkOptions);
-
-		// centre screen on marker
-		var bounds = new google.maps.LatLngBounds();
-		if (place.geometry.viewport) {
-            // Only geocodes have viewport.
-            bounds.union(place.geometry.viewport);
-        }
-        else {
-            bounds.extend(place.geometry.location);
-        }
-
-		// fit map to bounds selected by place search
-		map.fitBounds(bounds);
-		// set zoom to street level
-		map.setZoom(15);
-
-		// get stop/search data from api
-		getStopsData();
-	});
-} */
-
-/*function addMarker(markerOptions) {
-	marker = new google.maps.Marker(markerOptions);
-	var marker = new google.maps.Marker(markerOptions);
-
-	// add info window for each stop
-
-	// format date/time
-	var date = new Date(stop.datetime);
-	var formattedDate = date.toString();
-
-	var infoContent = "<div id='infowindow'>"
-							+ '<p>' + formattedDate + '</p>'
-					   		+ '<p>' + stop.legislation + '</p>'
-					   		+ '<p>' + stop.gender + ', ' + stop.age_range + ', ' + stop.self_defined_ethnicity + '</p>'
-					   		+ '<p>' + 'result: ' + stop.outcome + '</p>'
-					   	+ '</div>';
-
-	// test
-	console.log(infoContent);
-
-	var infowindow = new google.maps.InfoWindow({
-		content: infoContent
-	});
-	marker.addListener('click', function() {
-		infowindow.open(map, marker);
-	})
-}; */
-
-/*function getStopsData() {
-	// get current bounds from getBounds object
-	var current_bounds = map.getBounds();
-	var min_lat = current_bounds.R.R;
-	var min_lng = current_bounds.j.j;
-	var max_lat = current_bounds.R.j;
-	var max_lng = current_bounds.j.R;
-
-	// get police.uk stop-search data for current bounds (https://data.police.uk/docs/method/stops-street/)
-	var poly_1 = min_lat.toString() + ',' + min_lng.toString() + ':';
-	var poly_2 = min_lat.toString() + ',' + max_lng.toString() + ':';
-	var poly_3 = max_lat.toString() + ',' + max_lng.toString(); // 4th poly not required as per documentation
-	var query = 'https://data.police.uk/api/stops-street?poly=' + poly_1 + poly_2 + poly_3;
-
-	var stops;
-
-	var request = new XMLHttpRequest();
-	request.open("GET", query); // async by default
-	request.addEventListener("load", function() {
-		// once loaded, do this..
-
-		stops = JSON.parse(request.responseText);
-
-		// test
-		// console.log(stops);
-
-		addStopsMarkers(stops);
-	});
-	request.send(null);
-} */
-
+/*
+ * adds a marker with specified icon at specified location
+ */
 function addMarker(location, icon) {
 	var markerOptions = {
 		map : map,
@@ -235,21 +132,22 @@ function addMarker(location, icon) {
 	return new google.maps.Marker(markerOptions);	
 }
 
+/*
+ * takes a json object of stops and creates a marker for each
+ */
 function addStopsMarkers(stops) {
 	for (stop of stops) {
 		
-		// TODO: markerOptions.animation = google.maps.Animation.DROP;
-		
+		// TODO: markerOptions.animation = google.maps.Animation.DROP;		
 
+		// convert coords returned by police.uk API into format required for google maps API
 		var stopLocation = {
 			lat : Number(stop.location.latitude), 
 			lng: Number(stop.location.longitude)
 		};
 
-		
-
+		// add each marker by pushing to array
 		stopMarkers.push(addMarker(stopLocation, blueIcon));
-
 	}
 }
 
